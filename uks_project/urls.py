29ffe412ad0dd56f07613e3dc6c04db113c68d13
vesnamilic <44587403+vesnamilic@users.noreply.github.com>
2020-02-17 15:31:46 +0100

    Added sign in with GitHub account
    Sign in with GitHub account added.

--- uks_project/urls.py
+++ uks_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import url
 
 urlpatterns = [
     path('', include('uks_app.urls')),
     path('admin/', admin.site.urls),
+    url(r'^oauth/', include('social_django.urls', namespace='social'))
 ]
